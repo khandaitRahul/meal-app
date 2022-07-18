c70f9737d60fd566c07e6d6e97e77d88c550d9bc
Rahul Khandait <khandaitrahul5319@gmail.com>
2022-07-18 17:43:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="fs-2 fw-bold text-center bg-info">
+    <div className="fs-1 fw-bold text-center bg-info">
       This is meal app
     </div>
   );
